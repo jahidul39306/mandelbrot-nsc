7258688f5d01925026138d4f7d27ef9b5a7c5743
jahidul39306 <jahidulislamananto@gmail.com>
2026-02-05 14:25:38 +0100

    Add mandelbrot skeleton

---
+++ mandelbrot - nsc/mandelbrot.py
@@ -0,0 +1,19 @@
+"""
+Mandelbrot Set Generator
+Author : Jahidul
+Course : Numerical Scientific Computing 2026
+"""
+def f(x):
+    """
+    Example function .
+    Parameters
+    ----------
+    x : float
+    Input value
+    Returns
+    -------
+    float
+    Output value
+    """
+    # TODO : Implement the algorithm
+    return x**2 + 1
