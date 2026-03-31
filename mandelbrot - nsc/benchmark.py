54ecf4ac308994769ec1115a583dbe964e8369c1
jahidul39306 <jahidulislamananto@gmail.com>
2026-03-31 14:30:14 +0200

    changes in te benchmark file for 4096

--- mandelbrot - nsc/benchmark.py
+++ mandelbrot - nsc/benchmark.py
@@ -7,6 +7,7 @@ from mini_project_1.mandelbrot_naive import mandelbrot_naive
 from mini_project_1.mandelbrot_numpy import mandelbrot_numpy
 from mini_project_2.mandelbrot_parallel_chunk_lec5_m2 import mandelbrot_parallel
 from mini_project_2.mandelbrot_parallel_chunk_lec6_m1 import mandelbrot_dask
+from mini_project_2.mandelbrot_lec7_m1 import mandelbrot_dask as mandelbrot_dask_strato
 
 
 def bench(fn, *args, runs=5):
@@ -20,7 +21,7 @@ def bench(fn, *args, runs=5):
 
 
 if __name__ == "__main__":
-    width, height = 1024, 1024
+    width, height = 4096, 4096
     n_workers = 8
     args = (-2, 1, -1.5, 1.5, width, height)
     args_parallel = (
@@ -42,6 +43,15 @@ if __name__ == "__main__":
         100,
         4,
     )  # 4 is the best n_chunks for dask local with 8 workers
+    args_dask_strato = (
+        width,
+        -2,
+        1,
+        -1.5,
+        1.5,
+        100,
+        4,
+    )
 
     t_naive = bench(mandelbrot_naive, *args)
     t_numpy = bench(mandelbrot_numpy, *args)
@@ -49,6 +59,7 @@ if __name__ == "__main__":
     t_naive_numba = bench(mandelbrot_naive_numba, *args)
     t_mandelbrot_parallel = bench(mandelbrot_parallel, *args_parallel)
     t_mandelbrot_dask_local = bench(mandelbrot_dask, *args_dask_local)
+    t_mandelbrot_dask_strato = bench(mandelbrot_dask_strato, *args_dask_strato)
 
     print(f"Naive: {t_naive:.3f} seconds")
     print(f"NumPy: {t_numpy:.3f} seconds, speedup: {t_naive / t_numpy :.2f}x")
@@ -61,4 +72,7 @@ if __name__ == "__main__":
     )
     print(
         f"Dask local: {t_mandelbrot_dask_local:.3f} seconds, speedup: {t_naive / t_mandelbrot_dask_local :.2f}x"
+    )
+    print(
+        f"Dask strato: {t_mandelbrot_dask_strato:.3f} seconds, speedup: {t_naive / t_mandelbrot_dask_strato :.2f}x"
     )
